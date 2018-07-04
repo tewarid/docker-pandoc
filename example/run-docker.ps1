bc9cf3280ca2d8380ab3f9a38f6a40acd07cfd69
Devendra Tewari <devendra.tewari@gmail.com>
2018-07-03 22:42:22 -0300

    #6 add example

---
+++ example/run-docker.ps1
@@ -0,0 +1,1 @@
+docker run --rm -i -t -v ${PWD}:/workdir -w /workdir tewarid/pandoc ./run-pandoc.sh
---
+++ example/run-docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --rm -i -t -v `pwd`:/workdir -w /workdir tewarid/pandoc ./run-pandoc.sh
